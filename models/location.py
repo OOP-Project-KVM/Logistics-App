d1df9612e3642380d8d1aad9eb8296994a5ea1d9
gviktor13 <vikgeo13@gmail.com>
2024-08-05 19:56:50 +0300

    location and package and kind of ready

--- models/location.py
+++ models/location.py
@@ -1,11 +1,79 @@
-# Attributes:
-# name: name of the location (string)
-# arrival_time: expected arrival time (datetime)
-# departure_time: departure time (datetime, only for the first location)
 
 
-# Methods:
-# __init__(self, name, arrival_time=None, departure_time=None): constructor to initialize the attributes.
+from datetime import datetime
+
+from models.package import LOCATIONS
 
 
-## TODO VIKTOR
+class Location:
+    DATE_FORMAT = "%d-%m-%Y %H:%M:%S"
+
+    def __init__(self, name: str, arrival_time_str: str = None, departure_time_str: str = None):
+        self.validate_name(name)
+        self._name = name
+        self._arrival_time = self.parse_datetime(arrival_time_str) if arrival_time_str else None
+        self._departure_time = self.parse_datetime(departure_time_str) if departure_time_str else None
+        self.validate_times(self._arrival_time, self._departure_time)
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def arrival_time(self):
+        return self._arrival_time
+
+    @property
+    def departure_time(self):
+        return self._departure_time
+
+    @staticmethod
+    def validate_name(name):
+        if name[:3].upper() not in LOCATIONS:
+            raise ValueError("We don't provide services there.")
+
+    @staticmethod
+    def parse_datetime(datetime_str: str) -> datetime:
+
+        try:
+            return datetime.strptime(datetime_str, Location.DATE_FORMAT)
+        except ValueError:
+            raise ValueError(f"Date-time must be in the format '{Location.DATE_FORMAT}'.")
+
+    @staticmethod
+    def validate_times(arrival_time, departure_time):
+        if arrival_time and not isinstance(arrival_time, datetime):
+            raise ValueError("Arrival time must be a datetime object.")
+        if departure_time and not isinstance(departure_time, datetime):
+            raise ValueError("Departure time must be a datetime object.")
+
+    def __str__(self):
+        arrival_str = self._arrival_time.strftime(Location.DATE_FORMAT) if self._arrival_time else 'No arrival expected'
+        departure_str = self._departure_time.strftime(Location.DATE_FORMAT) if self._departure_time else 'No departure planned'
+        return (f"---Location Details---\n"
+                f"Name: {self._name}\n"
+                f"Next arrival : {arrival_str}\n"
+                f"Next departure : {departure_str}")
+
+
+try:
+    loc1 = Location(
+        name="Sydney",
+        arrival_time_str="05-08-2024 10:30:00",
+
+    )
+    loc2 = Location(
+        name="Melbourne",
+        arrival_time_str="06-08-2024 09:00:00"
+
+    )
+
+    print(loc1)
+    print()
+
+    print(loc2)
+
+
+except ValueError as e:
+    print(e)
+
--- models/package.py
+++ models/package.py
@@ -1,12 +1,105 @@
-# Attributes:
-# id: unique identifier (int or string)
-# start_location: starting location (string)
-# end_location: ending location (string)
-# weight: weight in kg (float)
-# customer_contact: contact information for the customer (string)
+LOCATIONS = ["SYD", "MEL", "ADL", "ASP", "BRI", "DAR", "PER"]
+DISTANCE_TABLE = {
+    "SYD": {"MEL": 877, "ADL": 1376, "ASP": 2762, "BRI": 909, "DAR": 3935, "PER": 4016},
+    "MEL": {"SYD": 877, "ADL": 725, "ASP": 2255, "BRI": 1765, "DAR": 3752, "PER": 3509},
+    "ADL": {"SYD": 1376, "MEL": 725, "ASP": 1530, "BRI": 1927, "DAR": 3027, "PER": 2785},
+    "ASP": {"SYD": 2762, "MEL": 2255, "ADL": 1530, "BRI": 2993, "DAR": 1497, "PER": 2481},
+    "BRI": {"SYD": 909, "MEL": 1765, "ADL": 1927, "ASP": 2993, "DAR": 3426, "PER": 4311},
+    "DAR": {"SYD": 3935, "MEL": 3752, "ADL": 3027, "ASP": 1497, "BRI": 3426, "PER": 4025},
+    "PER": {"SYD": 4016, "MEL": 3509, "ADL": 2785, "ASP": 2481, "BRI": 4311, "DAR": 4025}
+}
 
 
-# Methods:
-# __init__(self, id, start_location, end_location, weight, customer_contact): constructor to initialize the attributes.
+def get_distance(start_location, end_location):
+    return DISTANCE_TABLE[start_location.upper()][end_location.upper()]
 
-## TODO VIKTOR
+
+class Package:
+    all_ids = set()
+    customer_info = {}
+
+    def __init__(self, id_pack, start_location: str, end_location: str, weight: float, customer_contact: str):
+        self.validate_id(id_pack)
+        self.validate_location(start_location)
+        self.validate_location(end_location)
+        self.validate_weight(weight)
+        self.validate_customer_contact(customer_contact)
+        self.validate_locations(start_location, end_location)
+
+        self._id_pack = id_pack
+        self._start_location = start_location
+        self._end_location = end_location
+        self._weight = weight
+        self._customer_contact = customer_contact
+        self.distance = get_distance(start_location, end_location)
+        Package.all_ids.add(id_pack)
+        Package.customer_info[id_pack] = customer_contact
+
+    @property
+    def id_pack(self):
+        return self._id_pack
+
+    @property
+    def start_location(self):
+        return self._start_location
+
+    @start_location.setter
+    def start_location(self, value):
+        self.validate_location(value)
+        self.validate_locations(self._end_location, value)
+        self._start_location = value
+
+    @property
+    def end_location(self):
+        return self._end_location
+
+    @end_location.setter
+    def end_location(self, value):
+        self.validate_location(value)
+        self.validate_locations(self._start_location, value)
+        self._end_location = value
+
+    @property
+    def weight(self):
+        return self._weight
+
+    @property
+    def customer_contact(self):
+        return self._customer_contact
+
+    @customer_contact.setter
+    def customer_contact(self, value):
+        self.validate_customer_contact(value)
+        self._customer_contact = value
+
+    def validate_id(self, id_pack):
+        if not isinstance(id_pack, str):
+            raise ValueError("ID must be a non-empty string.")
+        if id_pack in Package.all_ids:
+            raise ValueError(f"Shipment ID {id_pack} already exists.")
+
+    def validate_location(self, location):
+        if location.upper() not in LOCATIONS:
+            raise ValueError("We don't provide services at this location.")
+
+    def validate_weight(self, weight):
+        if not isinstance(weight, (int, float)) or weight <= 0:
+            raise ValueError("Weight must be a positive number.")
+
+    def validate_customer_contact(self, customer_contact):
+        if not 5 <= len(customer_contact) <= 20:
+            raise ValueError("Customer contact info  must be between 5 and 20 characters!")
+
+    def validate_locations(self, start_location, end_location):
+        if start_location.upper() == end_location.upper():
+            raise ValueError("The start and end address must be in different locations.")
+
+    def __str__(self):
+        return (f"Package Details:\n"
+                f"ID: {self.id_pack}\n"
+                f"From: {self.start_location}\n"
+                f"To: {self.end_location}\n"
+                f"Distance: {self.distance}km\n"
+                f"Weight: {self.weight} kg\n"
+                f"Customer Contact: {self.customer_contact}")
+
