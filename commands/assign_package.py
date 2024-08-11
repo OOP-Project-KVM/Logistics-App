6eae3a40cf22241392477d5d31003ee305642168
gviktor13 <vikgeo13@gmail.com>
2024-08-11 18:36:47 +0300

    assign and create package command ready

--- commands/assign_package.py
+++ commands/assign_package.py
@@ -4,4 +4,25 @@
 
 # Output: Confirmation of package assignment to the route.
 
-# VIKTOR
+# VIKTORю
+from commands.base_command import BaseCommand
+
+
+
+class AssignPackageCommand(BaseCommand):
+
+    def execute(self):
+        package_id = int(self.params[0])
+        route_id = int(self.params[1])
+
+        route = self.app_data.get_route_by_id(route_id)
+
+        if route is None:
+            return f"Error: Truck with ID {route_id} not found."
+
+        package = self.app_data.get_package_by_id(package_id)
+        if package is None:
+            return f"Error: Package with ID {package_id} not found."
+
+        route.assign_package(package)
+        return f"Package {package.id} assigned to route {route.id}."
--- commands/create_package.py
+++ commands/create_package.py
@@ -6,3 +6,22 @@
 # Output: Confirmation of package creation with unique ID.
 
 #VIKTOR
+from commands.base_command import BaseCommand
+from models.package import Package
+
+
+class CreatePackageCommand(BaseCommand):
+    def execute(self):
+        if len(self.params) != 5:
+            return "Error: Invalid number of parameters. Expected 5 parameters."
+
+        id_pack = self.params[0]
+        start_location = self.params[1].upper()
+        end_location = self.params[2].upper()
+        weight = float(self.params[3])
+        customer_contact = self.params[4]
+
+        new_package = Package(id_pack, start_location, end_location, weight, customer_contact)
+        self.app_data.create_package(new_package)
+
+        return f"Package {id_pack} created successfully."
